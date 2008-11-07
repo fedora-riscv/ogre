2872e935b8d03fa26c5f94452776fd904171e81e
Alexey Torkhov <atorkhov@fedoraproject.org>
2008-11-07 10:39:19 +0000

    - New upstream release 1.6.0 - Updated samples running script - Removed non-free quake map from samples media - Added docs license in License tag

--- ogre-samples.sh
+++ ogre-samples.sh
@@ -1,4 +1,35 @@
-#!/bin/bash
+#!/bin/sh
+
+selectPCZ()
+{
+  sed -i 's|^\(Plugin=Plugin_OctreeSceneManager.so\)|#\1|' plugins.cfg
+  sed -i 's|^#\(Plugin=libPlugin_PCZSceneManager.so\)|\1|' plugins.cfg
+  sed -i 's|^#\(Plugin=Plugin_OctreeZone.so\)|\1|' plugins.cfg
+}
+
+unselectPCZ()
+{
+  sed -i 's|^#\(Plugin=Plugin_OctreeSceneManager.so\)|\1|' plugins.cfg
+  sed -i 's|^\(Plugin=libPlugin_PCZSceneManager.so\)|#\1|' plugins.cfg
+  sed -i 's|^\(Plugin=Plugin_OctreeZone.so\)|#\1|' plugins.cfg
+}
+
+runSample()
+{
+  sample=$1
+  if [ "$sample" = "PCZTestApp" ]; then
+    selectPCZ
+  else
+    unselectPCZ
+  fi
+
+  echo "Running $sample..."
+  $LIBDIR/OGRE/Samples/$sample
+
+  if [ "$sample" = "PCZTestApp" ]; then
+    unselectPCZ
+  fi
+}
 
 set -e
 
@@ -12,32 +43,30 @@ fi
 mkdir -p $HOME/.ogre-samples
 cd $HOME/.ogre-samples
 
-cp -f $LIBDIR/OGRE/Samples/resources.cfg .
-cp -f $LIBDIR/OGRE/Samples/media.cfg .
-
-echo "# Defines plugins to load" > plugins.cfg
-echo >> plugins.cfg
-echo "# Define plugin folder" >> plugins.cfg
-echo "PluginFolder=$LIBDIR/OGRE" >> plugins.cfg
-echo >> plugins.cfg
-echo "# Define D3D rendering implementation plugin" >> plugins.cfg
-for i in `(cd $LIBDIR/OGRE; ls *.so)`; do
-  if [ $i != libOgrePlatform.so ]; then
-    echo "Plugin=$i" >> plugins.cfg
-  fi
+for i in plugins.cfg media.cfg quake3settings.cfg resources.cfg; do
+  cp -f $LIBDIR/OGRE/Samples/$i .
 done
 
 set +e
 
-if [ $# -ge 1 ]; then
+if [ "$1" = "-a" ]; then
+  for i in `(cd $LIBDIR/OGRE/Samples/; find -type f -perm +111 | sort)`; do
+    runSample `echo $i | sed 's|./||'`
+  done
+elif [ $# -ge 1 ]; then
   while [ $# -ge 1 ]; do
-    $LIBDIR/OGRE/Samples/$1
+    runSample $1
     shift
   done
 else
-  for i in `(cd $LIBDIR/OGRE/Samples/; find -type f -perm +111)`; do
-    if [ $i != ./BSP ]; then
-      $LIBDIR/OGRE/Samples/$i
-    fi
+  echo "Usage: " `basename $0` " (samples | -a)"
+  echo
+  echo "samples - Runs specified samples from list"
+  echo "-a      - Runs all samples"
+  echo
+  echo -n "Available samples:"
+  for i in `(cd $LIBDIR/OGRE/Samples/; find -type f -perm +111 | sort)`; do
+    echo -n " $i" | sed 's|./||'
   done
+  echo
 fi
