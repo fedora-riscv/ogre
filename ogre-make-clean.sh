3262d27879a167703428ca58aaa60114b45e1ed4
Sérgio M. Basto <sergio@serjux.com>
2020-06-01 00:46:48 +0100

    Update to 1.12.6
    Use upstream source and simply remove the GL headers in %prep.
    
    Fix cmake build

--- ogre-make-clean.sh
+++ ogre-make-clean.sh
@@ -3,31 +3,30 @@
 set -e
 set -x
 
-version=1.9.0
+version=1.12.9
 
-[ ! -e ogre-${version} ]
-
-rpm -q mercurial
-hg clone https://bitbucket.org/sinbad/ogre ogre-${version}
-hg archive -R ogre-${version} -r v$(echo ${version} | tr . -) -t tbz2 ogre-${version}.tar.bz2
-rm -rf ogre-${version}
+[ ! -e ogre-${version} ] && wget https://github.com/OGRECave/ogre/archive/v${version}/ogre-${version}.tar.gz
 
 # Clean up
-tar xjf ogre-${version}.tar.bz2
+tar xf ogre-${version}.tar.gz
 pushd ogre-${version}
   # - Non-free licensed headers under RenderSystems/GL/include/GL removed
-  rm RenderSystems/GL/include/GL/{gl,glext,glxext,glxtokens,wglext}.h
+  rm RenderSystems/GL/include/GL/{gl,glext}.h
 
   # - GLEW sources updated
-  rpm -q glew-devel glew-debuginfo
+  # not working glew.c:1542:1: error: unknown type name 'PFNGLPROGRAMUNIFORM1DEXTPROC';
+  rpm -q glew-devel glew-debugsource
+  echo if package glew-debugsource is not installed, dnf debuginfo-install glew
   cp -f /usr/include/GL/{glew,glxew,wglew}.h RenderSystems/GL/include/GL/
   cp -f /usr/src/debug/glew-*/src/glew.c RenderSystems/GL/src/glew.cpp
   dos2unix RenderSystems/GL/src/glew.cpp
 
+
+  # https://github.com/OGRECave/ogre/issues/882
   # - Non-free chiropteraDM.pk3 under Samples/Media/packs removed
-  rm Samples/Media/packs/chiropteraDM.{pk3,txt}
+  # rm Samples/Media/packs/chiropteraDM.{pk3,txt}
 
   # - Non-free textures under Samples/Media/materials/textures/nvidia removed
-  rm Samples/Media/materials/textures/nvidia/*
+  # rm Samples/Media/materials/textures/nvidia/*
 popd
 tar cjf ogre-${version}-clean.tar.bz2 ogre-${version}
